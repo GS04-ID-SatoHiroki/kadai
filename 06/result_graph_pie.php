0b7d1d55d8385337bd3e85ac085c65c331d054b7
Hiroki Sato <sato.hiroki@live.com>
2016-05-05 17:06:01 +0900

    円グラフを追加

--- 06/result_graph_pie.php
+++ 06/result_graph_pie.php
@@ -5,7 +5,6 @@ include ("../../jpgraph/src/jpgraph_pie.php");
 
 // Create the graph. These two calls are always required
 $graph = new PieGraph(300, 200, "auto"); 
-$graph->SetFrame(true);
 
 //calls bellow are optional
 $graph->SetFrame(true);
@@ -20,21 +19,26 @@ $graph->title->Align("center","top");
 
 //include connection script to database:
 include("connection.php");
-$g = mysqli_query($dbc, "SELECT gender, COUNT(gender) as 'amount' FROM users GROUP BY gender");
-$data = array();
-while($row = mysqli_fetch_assoc($g)){
-    $data[] = $row['amount'];
+$result = mysqli_query($dbc, "SELECT gender, COUNT(gender) as 'amount' FROM users GROUP BY gender");
+
+
+$num = array();
+$leg = array();
+while($row = mysqli_fetch_object($result)){
+    array_push($num, $row->amount);
+    array_push($leg, $row->gender);
 //  'legends' => $row['gender'],
 //  'colors' => array('#0000FF', '#6600FF', '#CC00FF', '#66CC00', '#FFCC00')
 //    echo $row['gender'] . " : ". $row['amount'] . "<br />";
     
 }
+$data = array("leg" => $leg,"num" => $num);
+var_dump($data);die;
 
 //Graph data
 
-
-$pieplot = new PiePlot($data);
-//$pieplot->SetLegends($data['legends']);
+$pieplot = new PiePlot($data["num"]);
+$pieplot->SetLegends($data["leg"]);
 $pieplot->SetStartAngle(90);
 
 $graph->Add($pieplot);
